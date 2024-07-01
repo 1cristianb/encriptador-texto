7d88a461cb022f9422636c0b04b54db1f2e06aac
1cristianb <crboschi@alumno.unlam.edu.ar>
2024-07-01 11:28:42 -0300

    update se encripta el texto

--- encriptador.js
+++ encriptador.js
@@ -7,32 +7,56 @@ function encriptar() {
             document.getElementById("error").style.color = "red";
             return
         }
-        for (let index = 0; index < lista.length; index++) {
-            const element = lista[index];
-            console.log(element);
-        }
+        encriptarTexto(lista);
+        textoFinal=lista.join('');
         sinError();
-        mostarBotonCopiar();
+        mostarBotonCopiar(textoFinal);
     } else {
         sinTexto();
     }
 }
-
+function encriptarTexto(array) {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        switch (element) {
+            case 'a':
+                array[index] = 'ai';
+                break;
+            case 'e':
+                array[index] = 'enter';
+                break;
+            case 'i':
+                array[index] = 'imes';
+                break;
+            case 'o':
+                array[index] = 'ober';
+                break;
+            case 'u':
+                array[index] = 'ufat';
+                break;
+        }
+    }
+}
 function desencriptar() {
     console.log("Texto Desencriptado")
     mostarBotonCopiar();
 }
-function mostarBotonCopiar() {
+function mostarBotonCopiar(texto) {
     document.getElementById("primerMensaje").style.display = "none";
     document.getElementById("copiar").style.display = "block";
     document.getElementById("btn-copiar").style.display = "block";
-    document.getElementById("muestra").style.justifyContent = "end";
+    document.getElementById("muestra").style.justifyContent = "space-between";
+    document.getElementById("textoEncriptado").style.display = "block";
+    document.getElementById("textoEncriptado").innerHTML=texto;
 }
 
 function sinTexto() {
     document.getElementById("primerMensaje").style.display = "block";
+    document.getElementById("btn-copiar").style.display = "none";
+    document.getElementById("muestra").style.justifyContent = "center";
+    document.getElementById("textoEncriptado").style.display = "none";
 }
 
-function sinError(){
+function sinError() {
     document.getElementById("error").style.color = "#495057";
 }
